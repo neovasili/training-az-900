c6a986d6317c2fc619e66857c65cffc77a1a8103
Juan Manuel Ruiz Fernández <jruizferprofesional@gmail.com>
2025-10-26 09:12:21 +0100

    Add contents to the container

--- lp2-m2-u3-storage/index.ts
+++ lp2-m2-u3-storage/index.ts
@@ -1,8 +1,10 @@
+import { interpolate, asset } from "@pulumi/pulumi";
 import { ResourceGroup } from "@pulumi/azure-native/resources";
 import {
   StorageAccount,
   SkuName,
   BlobContainer,
+  Blob,
   Kind,
   PublicNetworkAccess,
   PublicAccess,
@@ -21,19 +23,56 @@ const resourceGroup = new ResourceGroup("IntroAzureRG", {
 });
 
 // Storage Account
-const storageAccount = new StorageAccount("introazurestorage", {
-  accountName: "neovasilistorage",
-  resourceGroupName: resourceGroup.name,
-  sku: {
-    name: SkuName.Standard_LRS,
+const storageAccount = new StorageAccount(
+  "introazurestorage",
+  {
+    accountName: "neovasilistorage",
+    resourceGroupName: resourceGroup.name,
+    sku: {
+      name: SkuName.Standard_LRS,
+    },
+    kind: Kind.StorageV2,
+    publicNetworkAccess: PublicNetworkAccess.Enabled,
+    allowBlobPublicAccess: true,
+    // If this is true, we don't have access to the container contents
+    allowSharedKeyAccess: false,
+    tags,
   },
-  kind: Kind.StorageV2,
-  publicNetworkAccess: PublicNetworkAccess.Enabled,
-  tags,
-}, { parent: resourceGroup });
+  { parent: resourceGroup },
+);
 
-const blobContainer = new BlobContainer("introcontainer", {
-  accountName: storageAccount.name,
-  resourceGroupName: resourceGroup.name,
-  publicAccess: PublicAccess.None,
-}, { parent: storageAccount });
+const blobContainer = new BlobContainer(
+  "introcontainer",
+  {
+    accountName: storageAccount.name,
+    resourceGroupName: resourceGroup.name,
+    publicAccess: PublicAccess.Blob,
+  },
+  { parent: storageAccount },
+);
+
+const manifest = new Blob(
+  "manifest",
+  {
+    accountName: storageAccount.name,
+    resourceGroupName: resourceGroup.name,
+    containerName: blobContainer.name,
+    blobName: "manifest.json",
+    contentType: "application/json",
+    source: new asset.StringAsset(
+      JSON.stringify(
+        {
+          app: "Intro to Azure",
+          version: "1.0.0",
+          description: "A sample application demonstrating Azure services.",
+        },
+        null,
+        0,
+      ),
+    ),
+  },
+  { parent: blobContainer },
+);
+
+export const blobContainerUrl = interpolate`https://${storageAccount.name}.blob.core.windows.net/${blobContainer.name}`;
+export const manifestUrl = interpolate`${blobContainerUrl}/${manifest.name}`;
